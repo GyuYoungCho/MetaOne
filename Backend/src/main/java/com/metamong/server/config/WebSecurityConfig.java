2ce9b82c4c6e243f2e7886582aa8ece6e6b71070
jinu0124 <jinwoo6612@naver.com>
2021-10-18 12:41:48 +0900

    Init Server

--- Backend/src/main/java/com/metamong/server/config/WebSecurityConfig.java
+++ Backend/src/main/java/com/metamong/server/config/WebSecurityConfig.java
@@ -1,12 +1,12 @@
 package com.metamong.server.config;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-import org.springframework.web.cors.CorsUtils;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.builders.WebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+//import org.springframework.web.cors.CorsUtils;
 
 //@Configuration
 //@EnableWebSecurity
