2393873e16a701a18aba3bd03cf15d82ffd7d62c
1996skdms <1996skdms@naver.com>
2021-10-19 22:50:54 +0900

    [BE] feat: :sparkles: certificate entity 추가

---
+++ Backend/src/main/java/com/metamong/server/entity/Certificate.java
@@ -0,0 +1,33 @@
+package com.metamong.server.entity;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
+public class Certificate extends BaseEntity{
+
+    @ManyToOne
+    @JoinColumn(name="user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name="education_id")
+    private Education education;
+
+    private Date createAt;
+
+    private Date passTime;
+
+    @Column(name="is_educated", columnDefinition = "TINYINT", length=4)
+    private int isEducated;
+
+    @Column(name="is_authenticated", columnDefinition = "TINYINT", length=4)
+    private int isAuthenticated;
+
+}
