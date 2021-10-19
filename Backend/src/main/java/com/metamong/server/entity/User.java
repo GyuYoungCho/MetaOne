7cc43819247b4691b6c382451201f6a4c6098795
yjyjk20 <yjyjk20@naver.com>
2021-10-19 20:10:08 +0900

    [BE] feat: :sparkles: user 테이블 entity 생성

---
+++ Backend/src/main/java/com/metamong/server/entity/User.java
@@ -0,0 +1,31 @@
+package com.metamong.server.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+@Getter
+@Setter
+public class User extends BaseEntity {
+
+    @ManyToOne
+    @JoinColumn(name = "room_id")
+    int roomId;
+
+    String email;
+    String password;
+    String fileUrl;
+
+    @Column(name="auth", columnDefinition = "TINYINT", length=4)
+    int auth;
+    String nickname;
+    String name;
+
+    @Column(name="state", columnDefinition = "TINYINT", length=4)
+    int state;
+}
