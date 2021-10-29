84a826e8433fe2b6b84864fc4f6543eaf7498ef6
GyuYoungCho <rbud613@gmail.com>
2021-10-29 17:45:09 +0900

    [FE] feat: :sparkles: message store

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -3,6 +3,8 @@ import Vuex from "vuex";
 import createPersistedState from "vuex-persistedstate";
 import guestbook from "./modules/guestbook.js";
 import join from "./modules/join.js";
+import message from "./modules/message.js";
+import education from "./modules/education.js";
 
 Vue.use(Vuex);
 
@@ -10,11 +12,9 @@ const store = new Vuex.Store({
   modules: {
     guestbook,
     join,
+    message,
+    education,
   },
 });
 
 export default store;
-// export default new Vuex.Store({
-//   guestbook,
-//   join,
-// });
