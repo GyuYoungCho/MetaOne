5981da71e4ffe9f53cb4ba9d47e6ec8d2b3bbf06
yjyjk20 <yjyjk20@naver.com>
2021-11-08 11:12:11 +0900

    [Unity] feat: :sparkles: 미션 중 timer 구현

--- unity/Assets/Scripts/LoadScene.cs
+++ unity/Assets/Scripts/LoadScene.cs
@@ -6,6 +6,46 @@ using UnityEngine.SceneManagement;
 
 public class LoadScene : MonoBehaviour
 {
+//    private Text[] timeText = { "05", "00" };
+    private float LimitTime = 300;
+    public Text text_Timer;
+    private bool startTimer = false;
+    private int min, sec;
+
+    void Update()
+    {
+        // 미션 클리어 패널이 null 아닐 때 타이머 멈추기
+        if (GameObject.Find("MissionClear") != null) startTimer = false;
+
+        // 타이머
+        if (startTimer)
+        {
+            LimitTime -= Time.deltaTime;
+
+            min = (int)LimitTime / 60;
+            sec = ((int)LimitTime - min * 60) % 60;
+
+            if (LimitTime < 60) text_Timer.color = Color.red;   // 시간이 1분 미만 남았을 경우
+
+            if(min<= 0 && sec<=0)
+            {
+                text_Timer.text = "00 : 00";
+            }
+            else
+            {
+                if(sec>=60)
+                {
+                    min += 1;
+                    sec -= 60;
+                }
+                else
+                {
+                    text_Timer.text = "0" + min.ToString() + " : " + sec.ToString();
+                }
+            }
+        }
+    }
+
     public void ChangeFire()
     {
         SceneManager.LoadScene("Fire");
@@ -28,11 +68,16 @@ public class LoadScene : MonoBehaviour
         // 메인카메라에 movingCharacter 스크립트 추가
         GameObject mission = GameObject.Find("MainCamera");
         mission.AddComponent<MovingCharacter>();
+
+        // 타이머 시작
+        GameObject.Find("Canvas").transform.Find("Timer").gameObject.SetActive(true);
+        startTimer = true;
     }
 
     public void clearMission()
     {
         // 결과 서버에 전송
+//        아이디 & text_Timer.text
 
         // 메인 맵으로 이동
         SceneManager.LoadScene("Main");
