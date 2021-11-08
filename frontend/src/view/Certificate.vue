940feaea741db79cca5654d35da0455a3e0b0160
GyuYoungCho <rbud613@gmail.com>
2021-11-08 13:46:47 +0900

    [FE] design: :art: 교육내역 증명서 디자자인 수정

--- frontend/src/view/Certificate.vue
+++ frontend/src/view/Certificate.vue
@@ -5,14 +5,14 @@
       <div class="container-fluid cert-contain">
         <div class="m-contain cert_content" :class="{'show-Certificate':show_cert}">
           
-          <img class="trophy mt-3 mb-3" src="@/assets/image/trophy.png" alt="not">
+          <img class="trophy mt-2 mb-2" src="@/assets/image/trophy.png" alt="not">
           <p>성명 : {{name}}</p>
           <p>교육 내용 : {{selecteducation.education}} 교육</p>
           <p>교육 날짜 : {{m_pass_day(selecteducation.createAt)}}</p>
           <br>
-          <p>위 교육생은 상기 교육과정을 성실히 이수하였으므로
+          <p>위 교육생은 상기 교육과정을 성실히 이수하였으므로<br>
               교육 이수증을 수여합니다.</p>
-          <img class="stamp mt-4" src="@/assets/image/buttonImage.png" alt="not">
+          <img class="stamp mt-2" src="@/assets/image/buttonImage.png" alt="not">
         </div>
       </div>
       <div class="justify-content-center">
