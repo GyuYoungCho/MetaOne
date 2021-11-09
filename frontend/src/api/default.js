c966f82c990284f50267ef1a4e26ea655532c075
jinu0124 <jinwoo6612@naver.com>
2021-11-09 18:38:36 +0900

    [FE / BE] feat: :sparkles: Refresh 토큰 재발행 로직 추가

--- frontend/src/api/default.js
+++ frontend/src/api/default.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import store from "@/store/index.js"
 
 const _axios = axios.create({
-  baseURL: "https://k5a305.p.ssafy.io/api",
+  baseURL: "http://localhost:8080/api",
   timeout: 100000,
 });
 
@@ -10,6 +10,33 @@ _axios.interceptors.request.use(
   function (config) {
     config.headers['accesstoken'] = store.state.user.accessToken
     return config
+  }
+)
+
+_axios.interceptors.response.use(
+  function (response) {                 // 로그인 성공 시
+    if (response.headers.accesstoken) {
+      store.commit('user/SET_USER_ACCESSTOKEN', response.headers.accesstoken)
+    }
+    if (response.headers.refreshtoken) {
+      store.commit('user/SET_USER_REFRESHTOKEN', response.headers.refreshtoken)
+    }
+    return Promise.resolve(response)
+  },
+
+  async function (error) {
+    // 1. 토큰 만료 시, 토큰 refresh + 기존 요청
+    if (error.response.status == 400 && error.response.data.msg == 'needRefreshToken') {
+      const originalRequest = error.config
+      originalRequest.headers.refreshtoken = store.state.user.refreshToken
+      return _axios(originalRequest)
+    } else if (error.response.status == 409 && error.response.data.msg == 'refreshTokenExpired') {
+      // 2. 액세스, 리프레쉬 모두 만료 시 로그아웃
+      store.dispatch('user/logout')
+    } else if (error.response.status >= 500) {
+      this.$router.push({ name: 'ServerError'})
+    }
+    return Promise.reject(error)
   }
 )
 
