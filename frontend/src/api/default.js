7af68220cea6f5d36423d321988576bf94fdfe5d
jinu0124 <jinwoo6612@naver.com>
2021-11-11 14:21:47 +0900

    [ BE ] feat: :sparkles: API 추가, Entity Builder.Default 추가

--- frontend/src/api/default.js
+++ frontend/src/api/default.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import store from "@/store/index.js"
 
 const _axios = axios.create({
-  baseURL: "https://k5a305.p.ssafy.io:8080/api",
+  baseURL: "https://k5a305.p.ssafy.io/api",
   timeout: 100000,
 });
 
