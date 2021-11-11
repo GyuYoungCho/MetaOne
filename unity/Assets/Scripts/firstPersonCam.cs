416643106a083f8f74217e9ddb1cd6dc836598b9
1996skdms <1996skdms@gmail.com>
2021-11-11 11:15:33 +0900

    [Unity] feat: :sparkles: 1인칭 카메라로 변경

--- unity/Assets/Scripts/firstPersonCam.cs
+++ unity/Assets/Scripts/firstPersonCam.cs
@@ -39,37 +39,5 @@ public class firstPersonCam : MonoBehaviour
         // 이동량을 좌표에 반영
         transform.position += move * moveSpeed * Time.deltaTime;
 
-
-        // 첫번째 미션
-        pos = transform.position;
-        if(73.5 <= pos.x && pos.x <= 74.2 && -4.2 < pos.z && pos.z < -3)
-        {
-            Debug.Log("범위 안에 들어왔다!");
-            GameObject clikckedToggle = GameObject.Find("FirstToggle");
-            Toggle t = clikckedToggle.GetComponent(typeof(Toggle)) as Toggle;
-            t.isOn = true;
-
-            // 팝업창 추가
-            GameObject canvas = GameObject.Find("Canvas");
-
-            GameObject.Find("Canvas").transform.Find("Guide").gameObject.SetActive(true);
-            GameObject guideText = GameObject.Find("Guide").transform.Find("Text").gameObject;
-            Text pt = guideText.GetComponent(typeof(Text)) as Text;
-
-            pt.text = "지진이 발생했다!\n교탁 아래로 들어가 몸을 보호해야해";
-
-            // 3초 후 Guide 숨기기
-            Invoke("hideGuide", 3);
-
-            // 2번째 미션 추가
-            GameObject.Find("asset_int_backpack_orange_057").AddComponent<ClickBackpack>();
-        }
-
-    }
-
-    public void hideGuide()
-    {
-        if (GameObject.Find("Guide") != null)
-            GameObject.Find("Guide").SetActive(false);
     }
 }
