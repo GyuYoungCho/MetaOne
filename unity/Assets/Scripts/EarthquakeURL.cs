4eef8440ea67c4c8b1266591850defc8165a0cdf
1996skdms <1996skdms@gmail.com>
2021-11-16 00:10:55 +0900

    [Unity] feat: :sparkles: unity -> front 교육명 전달, 교육 이수 유무 전달 (지진)

--- unity/Assets/Scripts/EarthquakeURL.cs
+++ unity/Assets/Scripts/EarthquakeURL.cs
@@ -1,10 +1,18 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+using System.Runtime.InteropServices;
+
 public class EarthquakeURL : MonoBehaviour
 {
+    [DllImport("__Internal")]
+    private static extern void UnityEducationNameHook(string eduName);
+
+    [DllImport("__Internal")]
+    private static extern void UnityEducationAuthHook(string boolean);
+
     public GameObject btn;
 
     // Start is called before the first frame update
@@ -24,6 +32,12 @@ public class EarthquakeURL : MonoBehaviour
 
             if(GetComponent<BoxCollider>().Raycast(ray, out hit, 10000f))
             {
+                // unity -> front로 교육명 전달
+                UnityEducationNameHook("지진");
+
+                // unity -> front로 교육 이수 유무 전달
+                UnityEducationAuthHook("True");
+
                 Application.OpenURL("https://schoolsafe.kr/post/view?id=1526");
                 btn.SetActive(true);
             }
