601a20ab5f45b8d9a4107f70330efef72d619fd2
1996skdms <1996skdms@gmail.com>
2021-11-17 10:25:43 +0900

    [BE] fix: :bug: TimeZone 수정 (방명록 불러오는 시간)

--- Backend/src/main/java/com/metamong/server/SsafyApplication.java
+++ Backend/src/main/java/com/metamong/server/SsafyApplication.java
@@ -3,9 +3,20 @@ package com.metamong.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
 
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@EnableScheduling
 public class SsafyApplication {
+
+    @PostConstruct
+    void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(SsafyApplication.class, args);
     }
