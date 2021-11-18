8b94b815d6d292d0e4b2e7b5edf08b7535e33b87
GyuYoungCho <rbud613@gmail.com>
2021-11-18 15:42:16 +0900

    [BE] fix: :bug: 카카오계정으로 일반로그인시 처리

--- Backend/src/main/java/com/metamong/server/service/UnityService.java
+++ Backend/src/main/java/com/metamong/server/service/UnityService.java
@@ -1,8 +1,5 @@
 package com.metamong.server.service;
 
-import com.metamong.server.entity.Room;
-
 public interface UnityService {
     void setCharacter(int userId, int unityCharacter);
-    Room setRoom(int userId, String unityRoom);
 }
