5c679d3ab2ddf8e3400d90165df4b56b549a3703
GyuYoungCho <rbud613@gmail.com>
2021-11-19 11:53:46 +0900

    [FE] fix: :bug: 카카오 로그인 버튼 바꾸기

--- frontend/src/view/Login.vue
+++ frontend/src/view/Login.vue
@@ -5,32 +5,39 @@
             <img src="@/assets/image/metaone.gif" ref="캐릭터 공간" class="logo-image"/>
         </div>
         <div class="login_form">
-            <li class="input-label" v-for="(e, i) in titles" :key="i">
-                <div class="row mb-4">
-                    <div class="col-md-3">
-                    </div>
-                    <div class="col-md-5">
-                        <inputparam :title="e" :placeholderData="placeholderDatas[i]" @tryLogin="tryLogin"></inputparam>
-                    </div>
-                    <div class="col-md-3">
-                        <div style="float: left;" v-if="i == 0">
-                            <button class="btn yellow-btn" @click="tryLogin()">&nbsp;&nbsp;&nbsp;&nbsp;시작하기&nbsp;&nbsp;&nbsp;&nbsp;</button>
+            <div class="row">
+                <div class="col-md-8">
+                    <li class="input-label" v-for="(e, i) in titles" :key="i">
+                        <div class="row mb-4" >
+                            <div class="col-md-3">
+                            </div>
+                            <div class="col-md-9">
+                                <inputparam :title="e" :placeholderData="placeholderDatas[i]" @tryLogin="tryLogin"></inputparam>
+                            </div>
                         </div>
-                        <div style="float: left;" v-if="i == 1">
-                            <button class="btn yellow-btn" @click="kakaoLogin()">카카오 로그인</button>
-                        </div>
-                    </div>
-                    <div class="col-md-2">
+                    </li>
+                    <div class="row mb-4">
+                        <div class="col-md-3"></div>
+                        <button class="col-md-1 not-submit" style="text-align:left; width:100px;" @click="toJoin()">회원가입</button>
+                        <button class="col-md-2 not-submit" style="text-align:left; width:150px;" @click="toFindPw()">비밀번호 찾기</button>
+                        <div class="col-md-3"></div>
                     </div>
                 </div>
-            </li>
-        
-            <div class="row" >
-                <div class="col-md-3"></div>
-                <button class="col-md-1 not-submit" style="text-align:left; width:100px;" @click="toJoin()">회원가입</button>
-                <button class="col-md-2 not-submit" style="text-align:left; width:150px;" @click="toFindPw()">비밀번호 찾기</button>
+                <div class="col-md-2">
+                    <div class="row">
+                        <div style="float: left;">
+                            <button class="btn yellow-btn" @click="tryLogin()">&nbsp;&nbsp;시작하기&nbsp;&nbsp;</button>
+                        </div>
+                    </div>
+                    <div>
+                        <button class="kakao-btn mt-3" @click="kakaoLogin()">
+                            <img src="@/assets/image/kakao.png" class="kakao-img" />
+                        </button>
+                    </div>
+                </div>
             </div>
         </div>
+
     </div>
 </template>
 
